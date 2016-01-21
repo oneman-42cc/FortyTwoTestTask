e87632e8b445c68138b8ff3daca4c8dd1a4c0a5e
peter <oneman.42cc@gmail.com>
2016-01-21 17:55:59 +0200

    ticket:5 Modify the model Profile by adding photo, update home view to show photo.

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 admin.autodiscover()
 
@@ -9,4 +10,9 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url('^', include('django.contrib.auth.urls')),
+    (
+        r'^uploads/(?P<path>.*)$',
+        'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT},
+    ),
 )
