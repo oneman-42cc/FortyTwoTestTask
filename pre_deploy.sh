77e78d4843bf8a08fd5c9e2df6ebdff5e6988d28
peter <oneman.42cc@gmail.com>
2016-01-26 14:57:58 +0200

    Try resolve problem with migrations. 6

--- pre_deploy.sh
+++ pre_deploy.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+MANAGE=django-admin.py
+SETTINGS=fortytwo_test_task.settings
+
 cd static && ls && cd ..
 rm -rf static/*
 
 tree -p ../uploads
 rm -f ../uploads/*.png
 
-python manage.py migrate --delete-ghost-migrations
+PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=$SETTINGS $MANAGE migrate --delete-ghost-migrations
