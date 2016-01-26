6ad7d1686d579ac2e052027ad653885143ef091c
peter <oneman.42cc@gmail.com>
2016-01-26 14:24:27 +0200

    Try resolve problem with migrations.

--- apps/hello/__init__.py
+++ apps/hello/__init__.py
@@ -1,3 +1,12 @@
 # flake8: noqa
 
+
 import hello.signals
+from south.models import MigrationHistory
+
+stupied_migrations = MigrationHistory.objects.filter(
+    migration__in=["0003_auto__del_field_request_read", "0004_auto__add_field_profile_photo"],
+)
+
+import sys
+print >> sys.stderr, stupied_migrations
